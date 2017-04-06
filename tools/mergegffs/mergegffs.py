3035d3275b5f8be875ed90080cf55c4b08d00f21
Eduardo Alves <alves.eduardo@gmail.com>
2017-04-06 11:55:15 +0000

    uploaded to test shed

--- tools/mergegffs/mergegffs.py
+++ tools/mergegffs/mergegffs.py
@@ -9,12 +9,28 @@ def main(argv, wayout):
                 argv.append("-h")
         parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=__doc__)
         parser.add_argument('-m','--genemap', help="mapping of genes to transcripts")
-        parser.add_argument('-p','--peptides', help="peptide prediction gff")
-        parser.add_argument('-a','--annotation', help="annotation gff with protein coordinates",nargs="*")
-        parser.add_argument('-d','--database', help="gff database to load or create")
+        parser.add_argument('-p','--peptides',required=True, help="peptide prediction gff")
+        parser.add_argument('-b','--blastp', help="blastp outfmt6 results for peptides")
+        parser.add_argument('-f','--pfam', help="hmmer results for peptides")
+        parser.add_argument('-t','--tmhmm', help="tmhmm results for peptides")
+        parser.add_argument('-s','--signalP', help="signalP results for peptides")
+        parser.add_argument('-d','--database', required=True, help="gff database to load or create")
         args = parser.parse_args(argv)
+
+
         db = gffutils.create_db(args.peptides, dbfn=args.database, force=True, keep_order=True,merge_strategy='merge', sort_attribute_values=True)
-        for gfffile in args.annotation:
-                db=db.update(gfffile)
+        if args.blastp == True:
+            os.system("blast2genomegff.py -b "+args.blastp+" -g  -d "+args.peptides+" -p blastp -t protein_match -T  -x > blastp.gff")
+            db=db.update(args.blastp)
+        if args.blastp == True:
+            os.system("convert2gff.py -i "+args.signalP+" -g "+args.peptides+" -T -t signalP > signalp.gff")
+            db=db.update(args.blastp)
+        if args.blastp == True:
+            os.system("convert2gff.py -i "+args.tmhmm+" -g "+args.peptides+" -T -t tmhmm > tmhmm.gff")
+            db=db.update(args.blastp)
+        if args.blastp == True:
+            os.system("pfam2gff.py -i "+args.pfam+" -g "+args.peptides+" -T > PFAM.gff")
+            db=db.update(args.blastp)
+
 if __name__ == "__main__":
         main(sys.argv[1:],sys.stdout)
